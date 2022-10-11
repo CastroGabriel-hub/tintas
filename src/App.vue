b0d565f90bf334ec4b2321cb962de5b7e6aa87d4
gabriel <gabrielcastro.ifpr@gmail.com>
2022-10-10 22:58:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <label for="area">Informe a área a ser pintada: </label>
+  <input type="text" name="area" v-model="area">
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      area : 0,
+    }
+    
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
